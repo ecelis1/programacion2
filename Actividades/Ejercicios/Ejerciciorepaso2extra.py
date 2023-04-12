43dd2f948099690569908c54526897e9fa9f6a13
ecelis1 <emacelis1@gmail.com>
2023-04-12 08:46:57 -0300

    modficiacione en el codigo

--- Actividades/Ejercicios/Ejerciciorepaso2extra.py
+++ Actividades/Ejercicios/Ejerciciorepaso2extra.py
@@ -1,40 +1,36 @@
 import pprint
 import os
 
+
+#FUNCION LIMPIAR PANTALLA
 def limpiarpantalla():
     os.system('cls')
 
-# inicializar matriz de resultados con ceros
+#INICIALIZO LA MATRIZ CON 0s
 resultados = [[0 for j in range(5)] for i in range(5)]
 
-# pedir al usuario los resultados de cada partido
+#INGRESO LOS GOLES TANTOS A FAVOR COMO EN CONTRA
 for i in range(5):
     for j in range(i+1, 5):
         while True:
             try:
                 print(f'EQUIPO {i+1} vs EQUIPO {j+1}')
-                goles_i = int(input(f"Ingrese la cantidad de goles del equipo {i+1} en el partido contra el equipo {j+1}: "))
-                goles_j = int(input(f"Ingrese la cantidad de goles del equipo {j+1} en el partido contra el equipo {i+1}: "))
-                if goles_i < 0 or goles_j < 0:
+                goles_equipoa = int(input(f"Ingrese la cantidad de goles del equipo {i+1} en el partido contra el equipo {j+1}: "))
+                goles_equipob = int(input(f"Ingrese la cantidad de goles del equipo {j+1} en el partido contra el equipo {i+1}: "))
+                if goles_equipoa < 0 or goles_equipob < 0:
                     print("Error: el número de goles debe ser un entero positivo.")
                     continue
                 break
             except ValueError:
                 print("Error: debe ingresar un número entero positivo.")
         
-              
-        resultados[i][j] = goles_i
-        resultados[j][i] = goles_j
-        print()
-        print("""TABLA DE RESULTADOS\nP1 P2 P3 P4 P5""")
-        for f in range(5):
-            for c in range(5):
-                print(resultados[f][c],end='  ')
-            print()
-        print()
+        #ASIGNO LOS GOLES A LA MATRIZ RESULTADOS  
+        resultados[i][j] = goles_equipoa
+        resultados[j][i] = goles_equipob
 
 limpiarpantalla()
-print("""TABLA DE RESULTADOS\nFINAL\nP1 P2 P3 P4 P5""")
+#IMPRIMO TABLA RESULTADO FINAL
+print("\nTABLA DE RESULTADOS\nFINAL\nP1 P2 P3 P4 P5")
 for i in range(5):
     for j in range(5):
         print(resultados[i][j],end='  ')
@@ -61,9 +57,4 @@ for i in range(5):
         goles_recibidos += resultados[j][i]
     
     # imprimir estadísticas del equipo
-    print(f"""Equipo {i+1}:
-    Triunfos: {triunfos}
-    Empates: {empates}
-    Derrotas: {derrotas}
-    Goles marcados: {goles_marcados}
-    Goles recibidos: {goles_recibidos}\n""")
+    print(f"""Equipo {i+1}:\nTriunfos: {triunfos}\nEmpates: {empates}\nDerrotas: {derrotas}\nGoles marcados: {goles_marcados}\nGoles recibidos: {goles_recibidos}\n""")
